96794e60760baf4354c868a4d4b3274ae20878a3
MatinQurban <matinqurban@gmail.com>
2023-09-06 17:33:55 -0700

    Recipe book header file

---
+++ recipeBook.h
@@ -0,0 +1,29 @@
+// Header file for RecipeBook class
+// Description: This class holds a list of recipes.
+
+
+#ifndef RECIPEBOOK_H
+#define RECIPEBOOK_H
+
+#include "recipeWithPostInstructions.h"
+
+class RecipeBook
+{
+public:
+    RecipeBook();
+    RecipeBook(string name);
+
+    void setName(string name);
+    string getName();
+
+    void addRecipe(RecipeWithPostInstructions recipe);
+    void printRecipeBook();
+
+    Recipe getRecipe(string name);
+
+private:
+    string name;
+    vector<RecipeWithPostInstructions> recipes;
+};
+
+#endif
